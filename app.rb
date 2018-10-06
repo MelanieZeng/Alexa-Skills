45b7bb16e9568e8f484ff168053cf058340b1f56
MelanieZeng <37940713+MelanieZeng@users.noreply.github.com>
2018-10-06 11:10:27 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -65,11 +65,9 @@ post '/incoming/alexa' do
 end
 
 
-
 # ----------------------------------------------------------------------
 #     ERRORS
 # ----------------------------------------------------------------------
-
 
 
 error 401 do 
@@ -82,3 +80,49 @@ end
 # ----------------------------------------------------------------------
 
 private
+
+def update_status status, duration = nil
+  
+  # gets a corresponding message 
+  message = get_message_for status, duration
+  # posts it to slack
+  post_to_slack status, message
+  
+end 
+
+def get_message_for status, duration
+
+  # Default response
+  message = "other/unknown"
+  
+  # looks up a message based on the Status provided
+  if status == "HERE"
+    message = ENV['APP_USER'].to_s + " is in the office."
+  elsif status == "BACK_IN"
+    message = ENV['APP_USER'].to_s + " will be back in #{(duration/60).round} minutes"
+  elsif status == "BE_RIGHT_BACK"
+    message = ENV['APP_USER'].to_s + " will be right back"
+  elsif status == "GONE_HOME"
+    message = ENV['APP_USER'].to_s + " has left for the day. Check back tomorrow."
+  elsif status == "DO_NOT_DISTURB"
+    message = ENV['APP_USER'].to_s + " is busy. Please do not disturb."
+  end
+  
+  # return the appropriate message
+  message
+  
+end
+
+def post_to_slack status_update, message
+  
+  # look up the Slack url from the env
+  slack_webhook = ENV['SLACK_WEBHOOK']
+  
+  # create a formatted message 
+  formatted_message = "*Status Changed for #{ENV['APP_USER'].to_s} to: #{status_update}*\n" 
+  formatted_message += "#{message} " 
+  
+  # Post it to Slack
+  HTTParty.post slack_webhook, body: {text: formatted_message.to_s, username: "OutOfOfficeBot", channel: "back" }.to_json, headers: {'content-type' => 'application/json'}
+  
+end 
